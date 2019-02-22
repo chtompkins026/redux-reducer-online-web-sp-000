66cccf4ca23c1ea60be66ecb1cca398279e13785
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-22 19:11:06 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,6 +1,11 @@
 export function manageFriends(state, action){
    switch(action.type){
-     case 'A'
-   }
+     case 'ADD_FRIEND':
+       return {friends: [...state.friends, action.friend]}; 
+     case 'REMOVE_FRIEND':
+       const out = state.friends.findIndex(friend => friend.id == action.id); 
+       return {friends: [...state.friends.slice(0, out), ...state.friends.slice(out + 1)]}; 
+      default: 
+        return state; 
   }
 }
